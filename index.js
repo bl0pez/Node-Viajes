e031ae2bc60212b6ae2c4ba6a29e00e8d8871f4f
b0pez <blopez4434@gmail.com>
2022-07-05 22:29:03 -0400

    add pug y routes

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const express = require('express');
+import express from 'express';
+import router from './routes/index.js';
 
 const port = process.env.PORT || 3000;
 
 
 const app = express();
+
+app.set('view engine', 'pug');
+
+app.use('/', router);
+
+
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
